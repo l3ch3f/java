5a39753a5a20f17990a645844a802d276d1056a0
Mc Fergus <mfsoeropaiman@gmail.com>
2020-12-20 14:27:49 +0100

    added float and doubles

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -12,6 +12,10 @@ public class Main {
     double myMinDoubleValue = Double.MIN_VALUE;
     double myMaxDoubleValue = Double.MAX_VALUE;
     System.out.println("Double minimum value = " + myMinDoubleValue);
-    System.out.println("Double maximum value = " + myMaxDoubleValue);    
+    System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+    int myIntValue = 5;
+    float myFloatValue = 5.25f;
+    double myDoubleBalue = 5.25d;
     }
 }
