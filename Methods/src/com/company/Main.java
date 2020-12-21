6af20d972d3b2a6651c5e9e281427a21280535b6
Mc Fergus <mfsoeropaiman@gmail.com>
2020-12-21 14:32:20 +0100

    Did 3 coding excercises

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,18 +4,55 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800,5,100);
-
-        calculateScore(true,10000,8,200);
+        int highScore = calculateScore(true, 800,5,100);
+        System.out.println("Your final score was " + highScore);
+//        calculateScore(true,10000,8,200);
+        int position = calculateHighscorePosition(1500);
+        displayHighScorePosition("Mark", position);
+        position = calculateHighscorePosition(900);
+        displayHighScorePosition("frank", position);
+        position = calculateHighscorePosition(300);
+        displayHighScorePosition("carol", position);
+        position = calculateHighscorePosition(50);
+        displayHighScorePosition("jim", position);
 
     }
     // Java kan je de method ook onderaan de main method zetten en oproepen in de main.
     // Je kan parameters geven om code niet te herhalen met de appropiate data type in de parameters.
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver)
         {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("your final score was " + finalScore);
+            finalScore += 2000;
+//            System.out.println("your final score was " + finalScore);
+            return finalScore;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public static void displayHighScorePosition (String player, int position) {
+        System.out.println(player + " managed to get into position " + position + " on the high score table");
+    }
+
+    public static int calculateHighscorePosition(int playerScore) {
+        if(playerScore >= 1000)
+        {
+            return 1;
+        }
+        else if (playerScore >= 500)
+        {
+            return 2;
+        }
+        else if (playerScore >= 100)
+        {
+            return 3;
+        }
+        else
+        {
+            return 4;
         }
     }
 }
