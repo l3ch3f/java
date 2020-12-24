dd05cb0efd6fc1b2edecacc2b8c18cb9977d3bab
Mc Fergus <mfsoeropaiman@gmail.com>
2020-12-24 17:44:35 +0100

    added ParsingValuesFromString

--- WhileAndDoWhile/src/com/company/Main.java
+++ WhileAndDoWhile/src/com/company/Main.java
@@ -66,7 +66,7 @@ public class Main {
                 continue;
             }
             System.out.println("Even number " + number);
-            
+
             evenNumbersFound++;
             if (evenNumbersFound >= 5) {
                 break;
