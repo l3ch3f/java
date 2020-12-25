59fe7e6af53880cbde8253980671abcf92309dc3
Mc Fergus <mfsoeropaiman@gmail.com>
2020-12-25 14:44:34 +0100

    Reading user inputs

---
+++ ReadingUserInput/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // System.in = buildin user input. new creates a new object.
+        Scanner scanner = new Scanner(System.in);
+
+        // scanner works also with integers you need to set the datatype.
+        // the reason why its above the String: it is recommende to call nextline or else it is gonna skip the next call.
+        System.out.println("Enter your year of birth:");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); // handle next line character (enter key)
+
+        // retrieve input from user and store it in a String.
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // calculate for how old a person is.
+        int age = 2020 - yearOfBirth;
+
+        // print outs to user.
+        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+
+
+        // it needs to be closed.
+        scanner.close();
+    }
+}
