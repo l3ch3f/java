2d9ee03c0152456241f2bfaae804dd6d0589097c
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-05 16:59:21 +0100

    .

--- Streams/src/com/l3ch3f/Main.java
+++ Streams/src/com/l3ch3f/Main.java
@@ -3,6 +3,23 @@ package com.l3ch3f;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String string = "I am a string. Yes, I am.";
+        System.out.println(string);
+        String yourString = string.replaceAll("I", "You");
+        System.out.println(yourString);
+
+        String aplhanumeric = "abcDeeeF123Ghhiiijkl99z";
+        System.out.println(aplhanumeric.replaceAll(".", "Y"));
+
+        System.out.println(aplhanumeric.replaceAll("^abcDeee","YYY"));
+
+        String secondString = "abcDeeeF123Ghhiiijkl99z";
+        System.out.println(secondString.replaceAll("^abcDeee","YYY"));
+
+        System.out.println(aplhanumeric.matches("^hello"));
+        System.out.println(aplhanumeric.matches("^abcDeee"));
+        System.out.println(aplhanumeric.matches("abcDeeeF123Ghhiiijkl99z"));
+
+        System.out.println(aplhanumeric.replaceAll("ijkl99z","THE END"));
     }
 }
