68edeaca0a5a4289b69b99eb56be1a839064536c
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-07 20:22:41 +0100

    Junit testing

---
+++ BankAccount/src/com/l3ch3f/BankAccountTest.java
@@ -0,0 +1,21 @@
+package com.l3ch3f;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    public void deposit() throws Exception {
+        fail("this test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void withdraw() throws Exception {
+        fail("this test has yet to be implemented");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getBalance() throws Exception {
+        fail("this test has yet to be implemented");
+    }
+}
