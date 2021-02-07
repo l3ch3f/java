5b9af90d9272794f76e3ded2262b242a8f87ae1b
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-07 21:46:52 +0100

    init testdb

---
+++ TestDb/src/com/l3ch3f/Main.java
@@ -0,0 +1,16 @@
+package com.l3ch3f;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/codejavadb? useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        } catch (SQLException e) {
+            System.out.println("Something went wrong " + e.getMessage());
+        }
+    }
+}
