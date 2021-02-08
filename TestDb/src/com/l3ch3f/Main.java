9bec4d8026a534471276dab6d7f41d7553c7b896
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-08 22:17:03 +0100

    .

--- TestDb/src/com/l3ch3f/Main.java
+++ TestDb/src/com/l3ch3f/Main.java
@@ -3,12 +3,21 @@ package com.l3ch3f;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
 	    try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/codejavadb? useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","Welkom01!");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                                "VALUES('Mc Fergus', 12356231, 'mcfergus@example.com' )");
+
+            statement.close();
+            conn.close();
+
         } catch (SQLException e) {
             System.out.println("Something went wrong " + e.getMessage());
         }
