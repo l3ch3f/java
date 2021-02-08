9d98ddade6f0a3c1af76879ae7a126e97a85840f
l3ch3f <mfsoeropaiman@gmail.com>
2021-02-08 22:40:48 +0100

    .CRUD db

--- TestDb/src/com/l3ch3f/Main.java
+++ TestDb/src/com/l3ch3f/Main.java
@@ -1,9 +1,6 @@
 package com.l3ch3f;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -12,8 +9,26 @@ public class Main {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/codejavadb? useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","Welkom01!");
             Statement statement = conn.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
+            /*
             statement.execute("INSERT INTO contacts (name, phone, email)" +
                                 "VALUES('Mc Fergus', 12356231, 'mcfergus@example.com' )");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                    "VALUES('Jane', 15356231, 'jane@example.com' )");
+            statement.execute("INSERT INTO contacts (name, phone, email)" +
+                    "VALUES('joe', 16231, 'joe@example.com' )");
+            */
+            /*
+            statement.execute("UPDATE contacts SET phone=123421 WHERE name='Jane'");
+
+            statement.execute("DELETE FROM contacts WHERE name='joe' ");
+            */
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while (results.next()) {
+                System.out.println(results.getString("name") + " " + results.getInt("phone")+ " " + results.getString("email"));
+            }
+            results.close();
 
             statement.close();
             conn.close();
